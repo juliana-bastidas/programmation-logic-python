cd8828d6d56ab11d91adc9289dd2155a360373d9
Juli <juli.bastidas12@gmail.com>
2024-06-07 16:47:21 -0500

    operadores y estructuras de control

---
+++ 01_operadores_estructuras.py
@@ -0,0 +1,25 @@
+'''
+OPERADORES
+'''
+
+# Operadores aritméticos   +, -, *, /, //, %, **
+sum = 10 + 3
+print (f"Suma: 10 + 3 = {10 + 3}")
+print (f"Suma 2: 10 + 3 = {sum}")
+
+print (f"Resta: 10 - 3 = {10 - 3}")
+print (f"Multiplicación: 10 * 3 = {10 * 3}")
+print (f"División: 10 / 3 = {10 / 3}")
+print (f"División entera: 10 // 3 = {10 // 3}")
+print (f"Modulo: 10 % 3 = {10 % 3}")
+print (f"Exponente: 10 ** 3 = {10 ** 3}") 
+
+# Operadores de comparación
+
+print (f"igualdad: 10 == 3 = {10 == 3}")
+print (f"Desigualdad: 10 != 3 = {10 != 3}")
+print (f"Mayor que: 10 > 3 = {10 > 3}")
+print (f"Menor que: 10 < 3 = {10 < 3}")
+print (f"Mayor o igual que: 10 >= 3 = {10 >= 3}")
+print (f"Menor o igual que: 10 <= 3 = {10 <= 10}")
+
